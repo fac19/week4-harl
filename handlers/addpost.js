770e927d8543249a7ea2c4844ce2e985f44ce16b
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-03-26 17:46:17 +0000

    Fix homepage.js addposts.js getposts.js Relates #23 Co-authored-by: @Roger-Heathcote

--- handlers/addpost.js
+++ handlers/addpost.js
@@ -8,24 +8,20 @@ function addPostHandler(request, response) {
   });
   // callback runs when request finishes and we have all the data
   request.on("end", () => {
-    console.log(body); // we should have the whole request body now
+    // we should have the whole request body now
     const data = new URLSearchParams(body);
     const username = data.get("username");
     const postTitle = data.get("post_title");
     const blogMessage = data.get("blog_message");
-    
     // pushing form data into model object
     increment()
     const id = getCount();
     const newPost = {
-
         author: username,
         title: postTitle,
         body: blogMessage
-  
     };
     posts[id]=newPost
-    console.log(posts)
     response.writeHead(302, { location: "/" }); // redirect to homepage with newpost added
     response.end();
   });

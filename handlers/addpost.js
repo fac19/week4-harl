9a59e3966fa749602de0fd2bee31c93fd65bfdf7
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-03-26 13:14:26 +0000

    Structure model object with form data and counter Relates #8 Co-authored-by: @Lizzy-j

--- handlers/addpost.js
+++ handlers/addpost.js
@@ -1,22 +1,31 @@
-const model = require("./model.js");
+const { counter, posts } = require("./model.js");
 
 function addPostHandler(request, response) {
   let body = "";
   // callback runs every time the stream has the next bit of data
   request.on("data", chunk => {
-    const data = new URLSearchParams(body);
-    const username = data.get("username");
-    const postTitle = data.get("post_title");
-    const blogMessage= data.get("blog_message");
     body += chunk;
-
   });
   // callback runs when request finishes and we have all the data
   request.on("end", () => {
     console.log(body); // we should have the whole request body now
-    response.writeHead(200, { "content-type": "text/html" });
+    const data = new URLSearchParams(body);
+    const username = data.get("username");
+    const postTitle = data.get("post_title");
+    const blogMessage= data.get("blog_message");
+    response.writeHead(302, { location: "/homepage" } ); // redirect to homepage with newpost added (there's a particular )
+
     // pushing form data into model object
-    response.end(); // redirect to homepage with newpost added
+        // counter = counter++; // test to see if counter is incrementing
+        // const id = counter;
+        // const posts = {
+        //   id: id,
+        //   author: username,
+        //   title: postTitle,
+        //   body: blogMessage
+        // };
+
+    response.end("/homepage.html"); 
   });
 }
 
@@ -24,5 +33,4 @@ module.exports = addPostHandler;
 
 // author, title and body
 
-// url 
-// username=john&post_title=hello&blog_message=world
+// url username=john&post_title=hello&blog_message=world
--- model.js
+++ model.js
@@ -1,5 +1,4 @@
  const counter = 0;
  const posts = {};
 
- module.exports.counter = counter;
- module.exports.posts = posts;
+ module.exports = { counter, posts };
